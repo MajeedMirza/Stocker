54c446ced27acbb74cd80ab951c4ed0ec555fb92
MajeedMirza <majeedmirza95@yahoo.com>
2017-01-16 04:33:40 -0500

    First commit

---
+++ Stocker/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Stocker
+{
+    partial class Stocker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Stocker));
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.Symbol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Current = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Range = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Change = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.perChange = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtSearch.Location = new System.Drawing.Point(12, 12);
+            this.txtSearch.MaxLength = 7;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(256, 22);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(274, 11);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowUserToAddRows = false;
+            this.dgv1.AllowUserToOrderColumns = true;
+            this.dgv1.AllowUserToResizeRows = false;
+            this.dgv1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv1.BackgroundColor = System.Drawing.Color.Lavender;
+            this.dgv1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgv1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Symbol,
+            this.Current,
+            this.Range,
+            this.Change,
+            this.perChange});
+            this.dgv1.Location = new System.Drawing.Point(12, 40);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.ReadOnly = true;
+            this.dgv1.RowHeadersVisible = false;
+            this.dgv1.RowTemplate.Height = 24;
+            this.dgv1.Size = new System.Drawing.Size(772, 251);
+            this.dgv1.TabIndex = 2;
+            this.dgv1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellDoubleClick);
+            // 
+            // Symbol
+            // 
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Symbol.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Symbol.HeaderText = "Symbol";
+            this.Symbol.Name = "Symbol";
+            this.Symbol.ReadOnly = true;
+            // 
+            // Current
+            // 
+            this.Current.HeaderText = "Current";
+            this.Current.Name = "Current";
+            this.Current.ReadOnly = true;
+            // 
+            // Range
+            // 
+            this.Range.HeaderText = "Range";
+            this.Range.Name = "Range";
+            this.Range.ReadOnly = true;
+            // 
+            // Change
+            // 
+            this.Change.HeaderText = "Change";
+            this.Change.Name = "Change";
+            this.Change.ReadOnly = true;
+            // 
+            // perChange
+            // 
+            this.perChange.HeaderText = "% Change";
+            this.perChange.Name = "perChange";
+            this.perChange.ReadOnly = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(355, 11);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Stocker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(796, 303);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Stocker";
+            this.Text = "Stocker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Stocker_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Symbol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Current;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Range;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Change;
+        private System.Windows.Forms.DataGridViewTextBoxColumn perChange;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Stocker/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Stocker
+{
+    public partial class Stocker : Form
+    {
+        string webData;
+        string pList;
+        public Stocker()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //pList = "\"\"";
+            //Properties.Settings.Default.StockList = pList;
+            //Properties.Settings.Default.Save();
+            this.Size = Properties.Settings.Default.Size;
+            pList = Properties.Settings.Default.StockList.ToUpper();
+            RefreshGrid();
+        }
+        
+        private void RefreshGrid()
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            string webStr = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(" + pList + ")%0A%09%09&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env";
+            webData = wc.DownloadString(webStr);
+            if (webData.Contains("Invalid environment specified") || webData.Contains("<LastTradePriceOnly/>")) //|| webData.Contains("<AskRealtime/>")
+            {
+                MessageBox.Show("Please enter a valid ticker symbol.");
+                pList = Properties.Settings.Default.StockList.ToUpper();
+                txtSearch.Text = "";
+                return;
+            }
+            dgv1.Rows.Clear();
+            Properties.Settings.Default.StockList = pList;
+            Properties.Settings.Default.Save();
+            string edit = webData;
+            int rownum = 0;
+            while (edit.Contains("<quote symbol=")) {
+                edit = edit.Remove(0, edit.IndexOf("<quote symbol=") + 15);
+                string symbol = "";
+                foreach (char c in edit)
+                {
+                    if(c != '"')
+                    {
+                        symbol += c;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                edit = edit.Remove(0, edit.IndexOf("<Change>") + 8);
+                string change = "";
+                foreach (char c in edit)
+                {
+                    if (c != '<')
+                    {
+                        change += c;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                edit = edit.Remove(0, edit.IndexOf("<LastTradePriceOnly>") + 20);
+                string Current = "";
+                foreach (char c in edit)
+                {
+                    if (c != '<')
+                    {
+                        Current += c;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                edit = edit.Remove(0, edit.IndexOf("<DaysRange>") + 11);
+                string range = "";
+                foreach (char c in edit)
+                {
+                    if (c != '<')
+                    {
+                        range += c;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                edit = edit.Remove(0, edit.IndexOf("<ChangeinPercent>") + 17);
+                string perChange = "";
+                foreach (char c in edit)
+                {
+                    if (c != '<')
+                    {
+                        perChange += c;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                string[] row1 = new string[] { symbol, Current, range, change, perChange};
+                dgv1.Rows.Add(row1);
+                if (change.Contains("-"))
+                {
+                    dgv1.Rows[rownum].Cells[3].Style.BackColor = Color.Salmon;
+                }
+                else
+                {
+                    dgv1.Rows[rownum].Cells[3].Style.BackColor = Color.LightGreen;
+                }
+                if (perChange.Contains("-"))
+                {
+                    dgv1.Rows[rownum].Cells[4].Style.BackColor = Color.Salmon;
+                }
+                else
+                {
+                    dgv1.Rows[rownum].Cells[4].Style.BackColor = Color.LightGreen;
+                }
+                rownum++;
+                dgv1.CurrentCell = null;
+                txtSearch.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgv1.Rows)
+            {
+               if(row.Cells[0].Value.ToString() == txtSearch.Text)
+                {
+                    //"YHOO" % 2C"AAPL" % 2C"GOOG" % 2C"MSFT" % 2C"NFLX" % 2C"TTWO" % 2C"TSLA" % 2C"CMG" % 2C"P" % 2C"REGN" % 2C"FB"""%2C
+                    pList = pList.Replace("%2C\"" + txtSearch.Text + "\"", "");
+                    //pList = pList.Replace(txtSearch.Text, "");
+                    Properties.Settings.Default.StockList = pList;
+                    Properties.Settings.Default.Save();
+                    dgv1.Rows.Remove(row);
+                    txtSearch.Text = "";
+                    return;
+                }
+            }
+            if (txtSearch.Text == "")
+            {
+                MessageBox.Show("Please enter a ticker symbol.");
+                return;
+            }
+            if (!(pList.Contains("%2C\"" + txtSearch.Text + "\"")))
+            {
+                pList += "%2C\"" + txtSearch.Text + "\"";
+            }
+            RefreshGrid();
+        }
+
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                btnSearch.PerformClick();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void dgv1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string myValue = dgv1[0, e.RowIndex].Value.ToString();
+            System.Diagnostics.Process.Start("https://ca.finance.yahoo.com/q?s=" + myValue);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
+
+        private void Stocker_ResizeEnd(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Size = this.Size;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            pList = "\"\"";
+            Properties.Settings.Default.StockList = pList;
+            Properties.Settings.Default.Save();
+            dgv1.Rows.Clear();
+        }
+    }
+}
